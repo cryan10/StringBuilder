d42f89caf18cd8712e205236074cf4f32dba7423
Carrie Ryan <cryan1205@gmail.com>
2017-03-07 21:41:00 -0500

    initial commit

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //StringBuilder allows for strings to be mutable
+
+
+            //Declare a new StringBuilder
+            StringBuilder builder = new StringBuilder();
+            builder.Append("The list starts here: ");
+            builder.AppendLine();
+            builder.Append("1 cat").AppendLine();
+
+            //get a reference to the StringBuilder's buffer content
+            string innerString = builder.ToString();
+
+            //display with console
+
+            Console.WriteLine(innerString);
+        }
+    }
+}
